60c83a04081a3db9c007424d75806bc649a01f7e
Valentina Camacho <valen.camacho@gmail.com>
2012-04-07 18:24:30 -0400

    change redict to findings

--- web.js
+++ web.js
@@ -194,7 +194,8 @@ app.post('/home', function(request,response){
     entry.save();
     
 
-	response.redirect('/findings/'+ entry._id);
+	/* response.redirect(/finding/+ entry._id); */
+	response.redirect("findings.html"+ entry._id);
 	 
 });
 
