2a85f3f66f9c4215b28b786b5b34548598f1ac87
Valentina Camacho <valen.camacho@gmail.com>
2012-04-18 22:52:48 -0400

    padding 0

--- static/js/js-intro/jquery.scrollpath.js
+++ static/js/js-intro/jquery.scrollpath.js
@@ -103,7 +103,8 @@
 	/* The Path object serves as a context to "draw" the scroll path
 		on before initating the plugin */
 	function Path( scrollS, rotateS ) {
-		var PADDING = 40,
+		/* var PADDING = 40, */
+		var PADDING = 0,
 			scrollSpeed = scrollS,
 			rotationSpeed = rotateS,
 			xPos = 0,
