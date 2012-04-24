102a25a16e113a32f9c785473b5d9a0c850dfcc2
Valentina Camacho <valen.camacho@gmail.com>
2012-04-24 19:22:39 -0400

    reverse

--- static/js/interface.js
+++ static/js/interface.js
@@ -70,7 +70,7 @@ var findClosestRec = function() {
 		
 		}
 			
-		console.log( ' adjusted hunch # = ' + adjustedHunchRecNum + '   current winner is: ' + jQuery(currentClosestRec).data('hunchstars') + ", recommendationDiff " + recommendationDiff );
+		//console.log( 'adjusted hunch # = ' + adjustedHunchRecNum + '   current winner is: ' + jQuery(currentClosestRec).data('hunchstars') );
 	
 	} );
 	
