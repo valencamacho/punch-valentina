fe806ed32a8284400b14e4518f03dd9720fdefdd
Valentina Camacho <valen.camacho@gmail.com>
2012-05-07 16:57:15 -0400

    offset 200 to the reverse

--- web.js
+++ web.js
@@ -364,7 +364,7 @@ app.get('/recommendations/', function(request , response) {
     
     var url1 ="http://api.hunch.com/api/v1/get-recommendations/?auth_token=6275cb220d05d45e411be1358cdcd7a765a1c80b&topic_ids=list_movie&limit=20&offset=100";
     
-    var url2 = "http://api.hunch.com/api/v1/get-recommendations/?auth_token=6275cb220d05d45e411be1358cdcd7a765a1c80b&topic_ids=list_movie&limit=300&reverse"
+    var url2 = "http://api.hunch.com/api/v1/get-recommendations/?auth_token=6275cb220d05d45e411be1358cdcd7a765a1c80b&topic_ids=list_movie&limit=300&offset=200&reverse"
     
     // using the ASYNC module, we will request both urls at the same time with .parallel
     // both will get requested at the same time and when finished will 
