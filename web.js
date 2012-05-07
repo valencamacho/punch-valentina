55c2ea5fd4d528432d6f5a2e5fae3cb9e23d1ac0
Valentina Camacho <valen.camacho@gmail.com>
2012-05-07 15:39:07 -0400

    change movies to websites

--- web.js
+++ web.js
@@ -362,9 +362,9 @@ app.get('/recommendations/', function(request , response) {
     
     
     
-    var url1 ="http://api.hunch.com/api/v1/get-recommendations/?auth_token=6275cb220d05d45e411be1358cdcd7a765a1c80b&topic_ids=list_movie&limit=200&offset=100";
+    var url1 ="http://api.hunch.com/api/v1/get-recommendations/?auth_token=6275cb220d05d45e411be1358cdcd7a765a1c80b&topic_ids=list_website&limit=200&offset=100";
     
-    var url2 = "http://api.hunch.com/api/v1/get-recommendations/?auth_token=6275cb220d05d45e411be1358cdcd7a765a1c80b&topic_ids=list_movie&limit=300&reverse"
+    var url2 = "http://api.hunch.com/api/v1/get-recommendations/?auth_token=6275cb220d05d45e411be1358cdcd7a765a1c80b&topic_ids=list_website&limit=300&reverse"
     
     // using the ASYNC module, we will request both urls at the same time with .parallel
     // both will get requested at the same time and when finished will 
