6ef1bd47d2971491e4739af25be677e3359d63ac
Valentina Camacho <valen.camacho@gmail.com>
2012-05-10 11:30:47 -0400

    take out offset in the first url

--- web.js
+++ web.js
@@ -293,9 +293,9 @@ app.get('/recommendations/', function(request , response) {
     
     
     
-    var url1 ="http://api.hunch.com/api/v1/get-recommendations/?auth_token=6275cb220d05d45e411be1358cdcd7a765a1c80b&topic_ids=list_movie&limit=2";
+    var url1 ="http://api.hunch.com/api/v1/get-recommendations/?auth_token=6275cb220d05d45e411be1358cdcd7a765a1c80b&topic_ids=list_movie&limit=4";
     
-    var url2 = "http://api.hunch.com/api/v1/get-recommendations/?auth_token=6275cb220d05d45e411be1358cdcd7a765a1c80b&topic_ids=list_movie&limit=400&offset=100&reverse"
+    var url2 = "http://api.hunch.com/api/v1/get-recommendations/?auth_token=6275cb220d05d45e411be1358cdcd7a765a1c80b&topic_ids=list_movie&limit=400&offset=400&reverse"
     
     // using the ASYNC module, we will request both urls at the same time with .parallel
     // both will get requested at the same time and when finished will 
