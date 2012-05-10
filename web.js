1c15b96cf0f0855d83b81cb5d71ec2e4262514e4
Valentina Camacho <valen.camacho@gmail.com>
2012-05-10 17:20:46 -0400

    ehmmm

--- web.js
+++ web.js
@@ -293,9 +293,9 @@ app.get('/recommendations/', function(request , response) {
     
     
     
-    var url1 ="http://api.hunch.com/api/v1/get-recommendations/?auth_token=6275cb220d05d45e411be1358cdcd7a765a1c80b&topic_ids=list_movie&limit=3";
+    var url1 ="http://api.hunch.com/api/v1/get-recommendations/?auth_token=6275cb220d05d45e411be1358cdcd7a765a1c80b&topic_ids=list_movie&limit=6";
     
-    var url2 = "http://api.hunch.com/api/v1/get-recommendations/?auth_token=6275cb220d05d45e411be1358cdcd7a765a1c80b&topic_ids=list_movie&limit=300&offset=380&reverse"
+    var url2 = "http://api.hunch.com/api/v1/get-recommendations/?auth_token=6275cb220d05d45e411be1358cdcd7a765a1c80b&topic_ids=list_movie&limit=300&offset=300&reverse"
     
     // using the ASYNC module, we will request both urls at the same time with .parallel
     // both will get requested at the same time and when finished will 
